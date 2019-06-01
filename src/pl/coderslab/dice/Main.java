d135dd84bcce234c7fb32055f9a201fe906d55ce
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-06-01 13:27:06 +0200

    Dice: parametersValCorr & general corectness

--- src/pl/coderslab/dice/Main.java
+++ src/pl/coderslab/dice/Main.java
@@ -41,40 +41,48 @@ public class Main {
             if (!formatCorr)
                 continue;
 
-
             int indexD = line.indexOf('D');
             int indexPlus = line.indexOf('+');
             int indexMinus = line.indexOf('-');
 
             if ( (indexD == 0) && (indexPlus < 0) && (indexMinus < 0) ){
-                optDy(parameters, line);
-                break;
+                if (!optDy(parameters, line))
+                    continue;
             } else if ( (indexPlus < 0) && (indexMinus < 0) ){
-                optxD(parameters, line);
-                break;
+                if (!optxD(parameters, line))
+                    continue;
             } else if( (indexD == 0) ){
                 parameters[0] = 1;
                 line = line.replaceAll("D", "");
                 if (indexMinus < 0) {
-                    optExistZ(parameters, line, "\\+");
-                    break;
+                    if (!optExistZ(parameters, line, "\\+"))
+                        continue;
                 }
                 else if(indexPlus < 0){
-                    optExistZ(parameters, line, "-");
-                    break;
+                    if (!optExistZ(parameters, line, "-"))
+                        continue;
                 }
             } else {
                 String[] lineParts = line.split("D");
-                parameters[0] = Integer.parseInt(lineParts[0]);
+                try {
+                    parameters[0] = Integer.parseInt(lineParts[0]);
+                } catch (NumberFormatException e) {
+                    System.out.println("incorrect format! Must be xDy+z");
+                    continue;
+                }
 
                 if ( (indexMinus < 0) ){
-                    optExistZ(parameters, lineParts[1], "\\+");
-                    break;
+                    if (!optExistZ(parameters, lineParts[1], "\\+"))
+                        continue;
                 } else if ( (indexPlus < 0) ){
-                    optExistZ(parameters, lineParts[1], "-");
-                    break;
+                    if (!optExistZ(parameters, lineParts[1], "-"))
+                        continue;
                 }
             }
+
+            // check correctness of values
+            if (parametersValCorr(parameters))
+                break;
         }
 
         return parameters;
@@ -82,22 +90,41 @@ public class Main {
 
     // check correctness of data-format
     private static boolean checkFormatCorr(String line){
+        // if line not contains D
         if (line.indexOf('D') < 0){
             System.out.println("incorrect format! Must be xDy+z");
             return false;
         }
 
+        // if line starts with +
+        if (line.charAt(0) == '+') {
+            System.out.println("incorrect format! Must be xDy+z");
+            return false;
+        }
 
+        // if line contains something more than D, -, + and numbers
         try {
-            String correct = line.replaceFirst("D", "");
-            correct = correct.replaceFirst("\\+", "");
-            correct = correct.replaceFirst("-", "");
+            String correct = line.replaceAll("D", "");
+            if (correct.contains("+"))
+                correct = correct.replaceFirst("\\+", "");
+
+            // this conditions allow to print right error with line e.g. -5D4-5 (when we have - as first char)
+            else if (correct.contains("-")){
+                if (correct.charAt(0) == '-') {
+                    System.out.println("Error: You can't roll 0 or less times! Try again!");
+                    return false;
+                }
+                else
+                    correct = correct.replaceFirst("-", "");
+            }
+
             Integer.parseInt(correct);
         } catch (NumberFormatException e) {
             System.out.println("incorrect format! Must be xDy+z");
             return false;
         }
 
+        // if D haven't got a number beside
         try {
             Integer.parseInt(   String.valueOf(  line.charAt( line.indexOf('D') + 1 )  )   );
         } catch (NumberFormatException num) {
@@ -107,32 +134,74 @@ public class Main {
             System.out.println("incorrect format! Must be xDy+z");
             return false;
         }
+
+        // if everything is ok
         return true;
     }
 
     // getParam: option without x and without z
-    private static void optDy (int[] parameters, String line){
+    private static boolean optDy (int[] parameters, String line){
         parameters[0] = 1;
-        parameters[1] = Integer.parseInt( line.replace("D", "") );
+        try {
+            parameters[1] = Integer.parseInt( line.replace("D", "") );
+        } catch (NumberFormatException e) {
+            System.out.println("incorrect format! Must be xDy+z");
+            return false;
+        }
         parameters[2] = 0;
+        return true;
     }
 
     // getParam: option with x and without z
-    private static void optxD (int[] parameters, String line){
+    private static boolean optxD (int[] parameters, String line){
         String[] lineParts = line.split("D");
-        parameters[0] = Integer.parseInt(lineParts[0]);
-        parameters[1] = Integer.parseInt(lineParts[1]);
+        try {
+            parameters[0] = Integer.parseInt(lineParts[0]);
+            parameters[1] = Integer.parseInt(lineParts[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("incorrect format! Must be xDy+z");
+            return false;
+        }
         parameters[2] = 0;
+        return true;
     }
 
     // getParam: option with z
-    private static void optExistZ (int[] parameters, String line, String z){
+    private static boolean optExistZ (int[] parameters, String line, String z){
         String[] lineParts = line.split(z);
-        parameters[1] = Integer.parseInt(lineParts[0]);
-        if (z.equals("\\+"))
-            parameters[2] = Integer.parseInt(lineParts[1]);
-        else if ( z.equals("-") )
-            parameters[2] = -Integer.parseInt(lineParts[1]);
+        try {
+            parameters[1] = Integer.parseInt(lineParts[0]);
+            if (z.equals("\\+"))
+                parameters[2] = Integer.parseInt(lineParts[1]);
+            else if ( z.equals("-") )
+                parameters[2] = -Integer.parseInt(lineParts[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("incorrect format! Must be xDy+z");
+            return false;
+        }
+        return true;
+    }
+
+    // check correctness of values
+    private static boolean parametersValCorr (int[] parameters){
+        boolean correctness = true;
+
+        if (parameters[0] <= 0) {
+            System.out.println("Error: You can't roll 0 or less times! Try again!");
+            correctness = false;
+        } else if (parameters[1] != 3 &&
+            parameters[1] != 4 &&
+            parameters[1] != 6 &&
+            parameters[1] != 8 &&
+            parameters[1] != 10 &&
+            parameters[1] != 12 &&
+            parameters[1] != 20 &&
+            parameters[1] != 100){
+            System.out.println("I do not have such a cube. Type: D3, D4, D6, D8, D10, D12, D20 or D100");
+            correctness = false;
+        }
+
+        return correctness;
     }
 
     // print retrieved information
