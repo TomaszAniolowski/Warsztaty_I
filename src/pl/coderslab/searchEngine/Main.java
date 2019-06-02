1095d4cfd13f99f38d1526a00dd0db3eb9386774
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-06-02 11:31:52 +0200

    Search Engine: corrected loading words & add filter for words

--- src/pl/coderslab/searchEngine/Main.java
+++ src/pl/coderslab/searchEngine/Main.java
@@ -12,18 +12,33 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-import java.util.regex.PatternSyntaxException;
 
 public class Main {
 
     public static void main(String[] args) {
-        String fileNameWP = loadWords("https://www.wp.pl/");
-        String fileNameONET = loadWords("http://www.onet.pl/");
-        String fileNameO2 = loadWords("https://www.o2.pl/");
+        // set the path which You want to use for creating files
+        String filePath = new String("src/pl/coderslab/searchEngine/");
 
+        // load words from websites
+        String fileNameWP = loadWords("https://www.wp.pl/", filePath);
+        String fileNameONET = loadWords("http://www.onet.pl/", filePath);
+        String fileNameO2 = loadWords("https://www.o2.pl/", filePath);
+
+        // set the table of words which You want to use for filtering
+        String[] censorship = { "oraz", "ponieważ", "się", "lub", "albo", "więc",
+                                "dlatego", "aby", "być", "skąd", "był", "lecz",
+                                "było", "będę", "mam", "twój", "twoje", "temu",
+                                "była", "dla", "jakie", "nad", "nas", "jak", "tego",
+                                "jaką", "jakiego", "ale", "jaki", "cię"};
+
+        // filter the words
+        censureWords(filePath, fileNameO2, censorship);
+        censureWords(filePath, fileNameONET, censorship);
+        censureWords(filePath, fileNameWP, censorship);
     }
 
-    private static String loadWords(String websiteURL) {
+    // loading words
+    private static String loadWords(String websiteURL, String filePath) {
 
         // connect
         Connection connect = Jsoup.connect(websiteURL);
@@ -34,12 +49,13 @@ public class Main {
         // create a file name
         String fileName = new String("popular_words_" + websiteName + ".txt");
 
+
         try {
             // loading data
             Document doc = connect.get();
             Elements links = doc.select("div");
-            File file = new File(fileName);
-            deleteExistingFile(fileName);
+            File file = new File(filePath+fileName);
+            deleteExistingFile(filePath+fileName);
 
             // signs for StringTokenizer
             String signs = new String(" .,/:;'\"[]{}-_+=-*0123456789");
@@ -79,14 +95,13 @@ public class Main {
                         }
 
                         // check if it is clean, if it is at least 3-character word and if it is already in a file
-                        if (clean && currentWord.length() >= 3 && !filerRepeat(fileName, currentWord)){
+                        if (clean && currentWord.length() >= 3 && !filerRepeat(filePath+fileName, currentWord)){
                             fw.append(currentWord + "\n");
                             // save changes
                             fw.close();
                         }
                         else
                             break;
-
 
                     }
                     // if it is clean keep going. if not repeat splitting this line with new sign-list
@@ -101,6 +116,17 @@ public class Main {
         return fileName;
     }
 
+    // getting the name of website
+    private static String getName(String websiteURL) {
+        String[] parts = websiteURL.split("\\.");
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].contains("www"))
+                return parts[i + 1];
+        }
+        return "unnamedSite";
+    }
+
+    // file deletion if exist
     private static void deleteExistingFile(String fileName){
         Path filePath = Paths.get(fileName);
         if (Files.exists(filePath))
@@ -111,6 +137,7 @@ public class Main {
             }
     }
 
+    // filter for repeated words
     private static boolean filerRepeat(String fileName, String word){
         File file = new File(fileName);
         try {
@@ -127,12 +154,48 @@ public class Main {
         return true;
     }
 
-    private static String getName(String websiteURL) {
-        String[] parts = websiteURL.split("\\.");
-        for (int i = 0; i < parts.length; i++) {
-            if (parts[i].contains("www"))
-                return parts[i + 1];
+    // filter the forbidden words
+    private static void censureWords(String filePath, String fileName, String[] censorship){
+        // bufer for list of words
+        StringBuilder list = new StringBuilder();
+        File file = new File(filePath+fileName);
+
+        try {
+            // input
+            Scanner inputFile = new Scanner(file);
+
+            // loading words from source file
+            while (inputFile.hasNextLine()){
+                String currentWord = inputFile.nextLine();
+                boolean censor = true;
+                // checking if current word is located in the table of forbidden words
+                for (String forbiddenWord: censorship)
+                    if (currentWord.equals(forbiddenWord)){
+                        censor = false;
+                        break;
+                    }
+
+                // add
+                if (censor)
+                    list.append(currentWord).append("\n");
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
         }
-        return "unnamedSite";
+
+        // target file name
+        String FiltredFileName = new String(filePath + "filtred_" + fileName);
+        File filtredFile = new File(FiltredFileName);
+
+        // inserting filter words to target file
+        try {
+            PrintWriter pw = new PrintWriter(filtredFile);
+            pw.print(list);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
